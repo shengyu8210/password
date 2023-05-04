ba1089ebf549df2fc17259bcadc6d262a522d162
ShengYu <shengyu8210@gmail.com>
2023-05-04 19:42:46 +0800

    update to final version

--- password.py
+++ password.py
@@ -6,8 +6,12 @@ while i > 0:
 		print('登入成功！')
 		break
 	else:
+		print('密碼錯誤！' )
 		i = i - 1
-		print('密碼錯誤！你還有', i ,'次機會' )
+		if i > 0:
+			print('你還有', i, '次機會！')
+		else:
+			print('沒機會了！你的帳號要被鎖住囉！')
 
 
 		
